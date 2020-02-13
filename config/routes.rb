5fd89ee18336bbe0109498e9a1193320e997c8ac
ulferts <jens.ulferts@googlemail.com>
2020-02-13 15:58:17 +0100

    add button to clear uploaded attachments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 OpenProject::Application.routes.draw do
   namespace :csv_import do
-    resource :work_packages, only: [:show, :create]
+    resource :work_packages, only: [:show, :create, :destroy]
     resource :mappings, only: [:show, :create]
   end
 end
