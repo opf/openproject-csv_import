4ede42a69e17c687637481d997493308aa5251d6
ulferts <jens.ulferts@googlemail.com>
2020-05-29 14:42:59 +0200

    use instance var

--- lib/open_project/csv_import/patches/journal_manager_patch.rb
+++ lib/open_project/csv_import/patches/journal_manager_patch.rb
@@ -5,18 +5,18 @@ module OpenProject::CsvImport::Patches::JournalManagerPatch
   included do
     def self.without_sending
       @without_sending = true
-      self.send_notification = false
+      @send_notification = false
       yield
     ensure
       @without_sending = false
-      self.send_notification = true
+      @send_notification = true
     end
 
     def self.reset_notification
       if @without_sending
-        self.send_notification = false
+        @send_notification = false
       else
-        self.send_notification = true
+        @send_notification = true
       end
     end
   end
